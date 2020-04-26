0ad7faa2c31fd999666d44ca4d7928ccd754cc46
Aravind Venugopal <36126351+Aravind1444@users.noreply.github.com>
2020-04-26 22:47:10 +0530

    Create user_input.java

---
+++ Day1/user_input.java
@@ -0,0 +1,18 @@
+public class exercise {
+  public static void main (String args[]) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter any number : ");
+    int a = scan.nextInt();
+    System.out.println("The number you have entered is  :" + a);
+  }
+}
+
+
+
+/*
+OUTPUT
+
+Enter any number : 5
+The number you have entered is 5
+
+*/
