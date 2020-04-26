a52a28bac4a3f8dbe6216dc70c2e554634c37343
Aravind Venugopal <36126351+Aravind1444@users.noreply.github.com>
2020-04-26 22:38:00 +0530

    Create Hello_World.java

---
+++ Day1/Hello_World.java
@@ -0,0 +1,7 @@
+//this program prints Hello World! as the output & obviously the kickstarter program :-)
+
+public class HelloWorld {
+  public static void main (String args[]) {
+    System.out.println("Hello World!");
+  }
+}
