989442dd7771ac5342fa5c8aa669a9370512c4c1
Aravind Venugopal <36126351+Aravind1444@users.noreply.github.com>
2020-04-26 22:48:25 +0530

    Create add_two_numbers.java

---
+++ Day1/add_two_numbers.java
@@ -0,0 +1,12 @@
+//Write a Java program to add two numbers.
+
+import java.util.Scanner;
+public class Addtwonumbers {
+
+	public static void main(String[] args) {
+		int num1=5, num2=15,sum;
+		sum = num1+num2;
+		System.out.println("The sum of the two numbers are : " + sum);
+	}
+
+}
