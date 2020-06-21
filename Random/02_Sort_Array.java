9b65ffd5443b9bbfe7b04d0b805856f9d60ebdc7
Aravind Venugopal <36126351+Aravind1444@users.noreply.github.com>
2020-06-21 22:34:42 +0530

    Update 02_Sort_Array.java

--- Random/02_Sort_Array.java
+++ Random/02_Sort_Array.java
@@ -1,33 +1,38 @@
-class SortArr {
-    public static void sortAsc(int[] arr) {                       //code to sort the array
-        int t = 0;
-        for (int i = 0; i < arr.length-1; i++) 
-        {
-            for (int j = i + 1; j < arr.length; j++) 
-            { //to get them compared with the current element 
-                if (arr[i] > arr[j]) 
-                { //the current element
-                    t = arr[i]; 
-                    arr[i] = arr[j]; 
-                    arr[j] = t; 
-                }
-            }
+class Main {
+
+  //code to sort the array
+
+  public static void sortArray(int[] arr) {                    
+    int t = 0;
+    for (int i = 0; i < arr.length-1; i++) {
+      for (int j = i + 1; j < arr.length; j++) { 
+        if (arr[i] > arr[j]) { 
+          t = arr[i]; 
+          arr[i] = arr[j]; 
+          arr[j] = t; 
         }
+      }
     }
+  }
     
-    //code to print the array before and after sorting
-    
-    public static void main( String args[] ) {
-        int array[] = {3, 2, 5, 7, 1};
-        System.out.println( "Array values before sorting : " );
-        for (int i =0 ; i < array.length; i++){
-            System.out.print(array[i]+ "  ");
-        }
-        System.out.println();
-        sortAsc(array);
-        System.out.println( "Array values after sorting : " );
-        for (int i =0 ; i < array.length; i++){
-            System.out.print(array[i]+ "  ");
-        }
+  //main method having input and printing output
+  //sort method is called in this  
+
+  public static void main( String args[] ) {
+    int array[] = {3, 2, 5, 7, 1};
+    System.out.print( "The input array is  : " );
+    for (int i =0 ; i < array.length; i++){
+      System.out.print(array[i]+ "  ");
     }
+    System.out.println();
+    System.out.println();
+    sortArray(array);     //calling the sortArray function 
+    System.out.print( "The Array after sorting in the ascending order is : " );
+    for (int i =0 ; i < array.length; i++){
+      System.out.print(array[i]+ "  ");
+    }
+  }  
 }     
+
+
+//try the code at : https://repl.it/repls/LankyAdequateDemoware#Main.java
