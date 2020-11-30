d8fdd3b9e0dc2fd855453b0ca1397225bbb783a5
Aravind Venugopal <36126351+Aravind1444@users.noreply.github.com>
2020-11-30 09:04:15 +0530

    Update 01_Two_Sum.java

--- Leetcode/01_Two_Sum.java
+++ Leetcode/01_Two_Sum.java
@@ -1,3 +1,5 @@
+//This is a brute forcce approach having time complexity of O(n^2)
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int arr[] = new int[2];                                 //we know that, the answer array will be having only 2 items since there will be only one pair of solution to meet target
@@ -10,5 +12,26 @@ class Solution {
             }
         }
         return arr;
+    }
+}
+
+
+// Now, if we want to optimize our solution, we can use HashMap for the same and bring down time to O(n) from O(n^2)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int result[] = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++){
+            int difference = target - nums[i];
+            if(map.containsKey(difference)){
+                    result[0] = i;
+                    result[1] = map.get(difference); 
+            }
+            else{
+                map.put(nums[i],i);
+            }
+        }
+        return result;
     }
 }
